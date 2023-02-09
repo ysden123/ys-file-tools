632e600c25d2cc4bea06e76976012d6fa4b243bb
Yuriy Stul <ysden123@gmail.com>
2023-02-09 13:22:29 +0200

    Extensions with count and percents

--- src/main/scala/com/stulsoft/file/tools/ext/ListAllExtensions.scala
+++ src/main/scala/com/stulsoft/file/tools/ext/ListAllExtensions.scala
@@ -13,18 +13,32 @@ import scala.io.StdIn.readLine
 import scala.util.{Failure, Success, Using}
 
 object ListAllExtensions:
-  def findAllExtensions(path: String): Either[String, String] =
-    val result = mutable.SortedSet[String]()
+  case class ExtensionInfo(name: String, count: Int)
 
+  def findAllExtensions(path: String): Either[String, String] =
     Using(Files.walk(Paths.get(path))) {
+      val extensionInfoMap = new mutable.HashMap[String, ExtensionInfo]
       stream =>
         stream
-          .map(path => FilenameUtils.getExtension(path.toString))
+          .map(path =>
+            FilenameUtils.getExtension(path.toString))
           .filter(extension => extension.nonEmpty)
-          .forEach(extension => result += extension)
+          .forEach(extension => {
+            val extensionInfo = extensionInfoMap.getOrElseUpdate(extension, ExtensionInfo(extension, 0))
+            extensionInfoMap.put(extension, ExtensionInfo(extension, extensionInfo.count + 1))
+          })
+        extensionInfoMap
+          .values
+          .toList
+          .sortBy(_.count)
+          .reverse
     } match
-      case Success(_) =>
-        Right(result.mkString("\n"))
+      case Success(extensionInfoList) =>
+        val total = extensionInfoList.map(_.count).sum
+        val result = extensionInfoList
+          .map(extensionInfo => f"${extensionInfo.name}%s\t${extensionInfo.count}%d\t${1.0 * extensionInfo.count * 100 / total}%2.2f%%")
+          .mkString("\n")
+        Right(result)
       case Failure(error) => error match
         case _: NoSuchFileException => Left(s"""Cannot find the "$path" directory.""")
         case exception: Exception => Left(exception.getMessage)
